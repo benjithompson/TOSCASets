7995fd75044d8432e6e1c6dea9b184625b898b15
Benjamin Thompson <b.thompson@tricentis.com>
2019-02-11 11:12:51 -0600

    Added Ribbon Addon

---
+++ addons/MyAddon/MyAddon/MyRibbonItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tricentis.TCAddOns;
+
+namespace MyAddon
+{
+    class MyRibbonItem : TCAddOnMenuItem
+    {
+        public override string ID => "RibbonItem1";
+
+        public override string MenuText => "MyAddOnItem";
+
+        public override void Execute(TCAddOnTaskContext context)
+        {
+            context.ShowMessageBox("Ribbon", "You clicked my ribbon item");
+        }
+    }
+}
