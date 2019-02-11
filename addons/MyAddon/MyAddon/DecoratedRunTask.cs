f5a4256a2fd77aefd612a1fc20114ab4ac40c161
Benjamin Thompson <b.thompson@tricentis.com>
2019-02-11 10:42:43 -0600

    AddOnTask working

---
+++ addons/MyAddon/MyAddon/DecoratedRunTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tricentis.TCAddOns;
+using Tricentis.TCAPIObjects.Objects;
+
+namespace MyAddon
+{
+    internal class DecoratedRunTask : TCAddOnTask
+    {
+        public override string Name => "RunMe";
+
+        public override Type ApplicableType => typeof(ExecutionEntry);
+
+        public override TCObject Execute(TCObject objectToExecuteOn, TCAddOnTaskContext taskContext)
+        {
+            taskContext.ShowMessageBox("Attention", String.Format("This entyr will be run via an AddOnTask: CWD: {0}", Environment.CurrentDirectory));
+            (objectToExecuteOn as ExecutionEntry).Run();
+            return null;
+        }
+    }
+}
---
+++ addons/MyAddon/MyAddon/MyAddon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tricentis.TCAddOns;
+
+namespace MyAddon
+{
+    public class MyAddon : TCAddOn
+    {
+        public override string UniqueName => "MyAddon";
+        public override string DisplayedName => "My Addon";
+    }
+}
